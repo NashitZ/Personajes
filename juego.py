0fe8903bbf6968e48c5f7b647a5438fde76039bf
NashitZ <116596606+NashitZ@users.noreply.github.com>
2022-12-12 16:56:41 -0300

    Create juego.py

---
+++ juego.py
@@ -0,0 +1,25 @@
+class Personajes:
+    def __init__(self,nickname,atack,defence,inteligence,live):
+    self.nickname=nickname
+    self.atack=atack
+    self.defence=defence
+    self.live=live
+
+class Tank(Personajes):
+    def __init__(self,nickname,atack,defence,inteligence,life=10000000000,fuerza)
+    super().__init__(nickname,atack,defence,inteligence,live)
+    self.fuerza=fuerza
+
+class Wizard(Personajes):
+    def __init__(self,nickname,atack,defence,inteligence,life,magia)
+    super().__init__(nickname,atack,defence,inteligence,live)
+    self.magia=magia
+
+def ganador(self):
+    if (live > 0):
+        print(f"El ganador es",{self.nickname})
+    else:
+        print("perdiste lol")
+
+
+
